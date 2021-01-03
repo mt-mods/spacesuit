c52500c4f4da2db5a9787d64aa191d41833088af
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-01-03 16:52:33 +0100

    increase time between drowning checks

--- drowning.lua
+++ drowning.lua
@@ -55,7 +55,7 @@ end
 local timer = 0
 minetest.register_globalstep(function(dtime)
 	timer = timer + dtime;
-	if timer >= 1 then
+	if timer >= 2 then
 		local t0 = minetest.get_us_time()
 
 		for _,player in ipairs(minetest.get_connected_players()) do
